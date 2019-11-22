ecf4023f9973fedd730dc545ada22887351f1ecd
SEIKIEE <33003026+SEIKIEE@users.noreply.github.com>
2019-11-23 02:41:23 +0800

    增加课程首页和课程内部首页

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var indexCourses = require('../models/statics/indexCourses');
+var indexCourses = require("../models/statics/indexCourses");
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index/index', { courses: indexCourses });
+router.get("/", function(req, res) {
+  res.render("messageBoard/titleBar", { courses: indexCourses });
 });
 
-router.get('/login', function (req, res) {
-  res.render('index/login');
+router.get("/login", function(req, res) {
+  res.render("index/login");
 });
 
-router.get('/regist', function (req, res) {
-  res.render('index/regist');
+router.get("/regist", function(req, res) {
+  res.render("index/regist");
 });
 
 module.exports = router;
