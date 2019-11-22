f4a2263b20061d45bf2a3027d72583b1b9373131
ycj <1052257712@qq.com>
2019-11-22 21:32:38 +0800

    新增用户头像功能

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,20 @@
 var User = require('../models/class/User');
+var svgCaptcha = require('svg-captcha');  //动态验证码核心库
+var indexCourses = require('../models/statics/indexCourses');
+
+/**
+ * 主页加载，区分登陆态和非登陆态
+ */
+exports.index = (req, res) => {
+    var token = req.session.token;
+    if (token) {
+        token.userPwd = null; //隐蔽用户密码
+        res.render('index/index', { courses: indexCourses, token: token });
+    } else {
+        res.render('index/index', { courses: indexCourses, token: null });
+    } 
+}
+
 
 /**
  * 用户身份校验，校验成功返回1，校验失败返回0
@@ -12,5 +28,46 @@ exports.userVerify = async (req, res) => {
     } else {
         req.session.token = ret[0];
         res.send({ status: 1 }).end();
+    }
+}
+
+/**
+ * 用户注销
+ */
+exports.userLogout = (req, res) => {
+    req.session.destroy();
+    res.send("<script>alert('注销成功'); window.location.href = '/';</script>").end();
+}
+
+/**
+ * 用户注册，注册成功返回1，注册失败返回0，验证码错误返回-1
+ */
+exports.userRegist = async (req, res) => {
+    if (!req.session.captcha || req.session.captcha != req.body.dynamic) {
+        res.send({ status: -1, msg: "验证码错误！" }).end();
+        return;
+    }
+    var ret = await User.prototype.regist(req.body);
+    if (ret == null) {
+        res.send({ status: 0, msg: "数据库异常！" }).end();
+    } else if (ret == 0) {
+        res.send({ status: 0, msg: "邮箱已被注册！" }).end();
+    } else {
+        res.send({ status: 1 }).end();
+    }
+}
+
+/**
+ * 生成动态验证码返回给前端
+ */
+exports.getCaptcha = (req, res) => {
+    try {
+        var captcha = svgCaptcha.create();
+        req.session.captcha = captcha.text; //生成动态码存到session中并返回给前端
+        console.log(req.session.captcha);
+        res.type('svg');
+        res.status(200).send(captcha.data);
+    } catch (err) {
+        console.log(err);
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var indexCourses = require('../models/statics/indexCourses');
 var userControllers = require('../controllers/userController');
 
 /* GET home page. */
-router.get('/', (req, res) => {
-  res.render('index/index', { courses: indexCourses });
-});
-
-router.get('/login', (req, res) => {
-  res.render('index/login');
-});
-
-router.get('/regist', (req, res) => {
-  res.render('index/regist');
-});
-
+router.get('/', userControllers.index);
+router.get('/login', (req, res) => { res.render('index/login'); });
+router.get('/logout', userControllers.userLogout);
+router.get('/regist', (req, res) => { res.render('index/regist'); });
+router.get('/regist/captcha', userControllers.getCaptcha);
 router.post('/login', userControllers.userVerify);
+router.post('/regist', userControllers.userRegist);
 
 module.exports = router;
