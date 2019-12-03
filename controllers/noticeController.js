8496c34aaec828499a907d607d33563db543e24f
SEIKIEE <33003026+SEIKIEE@users.noreply.github.com>
2019-12-04 00:38:33 +0800

    增加公告栏点击刷新功能

--- controllers/noticeController.js
+++ controllers/noticeController.js
@@ -1,5 +1,8 @@
 var Notice = require("../models/class/Notice");
 
+/**
+ * 获得进入通知页面，获得通知
+ */
 exports.getCourseNotice = async (req, res) => {
   var token = req.session.token;
   var noticeList = await Notice.prototype.getCourseNotice(1); //1应该用课程号替换掉
--- routes/notice.js
+++ routes/notice.js
@@ -4,5 +4,5 @@ var noticeController = require("../controllers/noticeController");
 
 /* GET notice page. */
 router.get("/", noticeController.getCourseNotice);
-
+router.post("/", noticeController.getCourseNotice);
 module.exports = router;
