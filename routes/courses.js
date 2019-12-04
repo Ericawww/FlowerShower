9c351b054594b9cd95c0c575f880b73b72ce163e
SEIKIEE <33003026+SEIKIEE@users.noreply.github.com>
2019-12-04 18:49:51 +0800

    无修改

--- routes/courses.js
+++ routes/courses.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var router = express.Router();
 var courseControllers = require("../controllers/courseController");
+var noticeControllers = require("../controllers/noticeController");
+var talkControllers = require("../controllers/talkController");
 
 router.get("/", courseControllers.getAllCourse); //courses
 
 router.get("/search", courseControllers.getCourseByName); //search?courseName=...
 router.get("/class/:id", courseControllers.getUserCoursePage);
-router.get("/class/:id/notice");
-router.get("/class/:id/talk");
+router.get("/class/:id/notice", noticeControllers.getCourseNotice);
+router.get("/class/:id/talk", talkControllers.getTalk);
 
 router.get("/:courseNumber", courseControllers.getCourseInfo); //courses/courseNumber
 
