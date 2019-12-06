20b64953c4fd07529990494aea2e4643cf0a6aa2
SEIKIEE <33003026+SEIKIEE@users.noreply.github.com>
2019-12-06 14:53:12 +0800

    zjy

--- routes/courses.js
+++ routes/courses.js
@@ -1,25 +1,25 @@
 var express = require("express");
 var router = express.Router();
 var courseControllers = require("../controllers/courseController");
-var classControllers = require("../controllers/classController");
+var classController = require("../controllers/classController");
 
 router.get("/", courseControllers.getAllCourse); //courses
 router.get("/search", courseControllers.getCourseByName); //search?courseName=...
 router.get("/class/:classID", courseControllers.getUserCoursePage);
-router.get("/class/:classID/notice", classControllers.getCourseNotice);
-router.get("/class/:classID/talk", classControllers.getTalk);
-router.get("/class/:classID/talk/:talkID", classControllers.showTalk);
-router.get("/class/:classID/talk/:talkID/return", classControllers.getTalk);
+router.get("/class/:classID/notice", classController.getCourseNotice);
+router.get("/class/:classID/talk", classController.getTalk);
+router.get("/class/:classID/talk/:talkID", classController.showTalk);
+router.get("/class/:classID/talk/:talkID/return", classController.getTalk);
 router.get("/:courseNumber", courseControllers.getCourseInfo); //courses/courseNumber
 
 router.post(
   "/class/:classID/notice/updateNotice",
-  classControllers.updateNotice
+  classController.updateNotice
 );
-router.post("/class/:classID/talk/updateTalk", classControllers.writeTalk);
+router.post("/class/:classID/talk/updateTalk", classController.writeTalk);
 router.post(
   "/class/:classID/talk/:talkID/updateComment",
-  classControllers.addComment
+  classController.addComment
 );
 //router.get('/courseGrade',courseControllers.getGrade);
 //router.get('/courseGradeChange', courseControllers.gradeChange);
