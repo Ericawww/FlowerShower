55ff6024615eb186d9cbddc29da40e432b24cb40
ycj <1052257712@qq.com>
2019-12-06 15:31:38 +0800

    update route/course

--- routes/courses.js
+++ routes/courses.js
@@ -19,10 +19,10 @@ router.post("/class/:classID/talk/:talkID/updateComment", classController.addCom
 
 
 //成绩
-router.get('/classes/:classID/teacher/courseGrade', courseController.getGrade);
-router.get('/classes/:classID/teacher/:classid/courseGradeChange', courseController.gradeChange);
-router.post('/classes/:classID/teacher/:classid/gradeChange', courseController.setGradeChange);
-router.post('/classes/:classID/teacher/:classid/gradeWeightChange', courseController.setGradeWeightChange);
+router.get('/class/:classID/teacher/courseGrade', courseController.getGrade);
+router.get('/class/:classID/teacher/courseGradeChange', courseController.gradeChange);
+router.post('/class/:classID/teacher/gradeChange', courseController.setGradeChange);
+router.post('/class/:classID/teacher/gradeWeightChange', courseController.setGradeWeightChange);
 
 //验证身份
 router.all("/class/:classID/student/", classController.checkStudent); // courses/classes/:classID/student  ->匹配学生，验证身份，next
@@ -36,10 +36,10 @@ router.get("/class/:classID/teacher/main", classController.getTcMainPage);
 router.get('/class/:classID/student/hw/all', classController.getStuAllHw);// courses/classes/:classID/student/hw/all ->作业列表
 router.all('/class/:classID/student/hw/:hw', classController.checkHw);//courses/classes/:classID/student/hw/:hw ->学生作业，验证作业，next
 router.get('/class/:classID/student/hw/:hw/detail', classController.getStuHwDetail);// courses/classes/:classID/student/hw/:hw/detail ->作业详情
-router.get('/class/:classID/student/hw/:hw/situation',classController.getStuHwSituation);// courses/classes/:classID/student/hw/:hw/situation ->提交和批改情况
-router.get('/class/:classID/student/hw/:hw/complain',classController.getStuHwComplain);// courses/classes/:classID/student/hw/:hw/complain ->申诉
-router.post('/class/:classID/student/hw/:hw/submitComplain',classController.submitComplain);//处理申诉
-router.get('/class/:classID/student/hw/:hw/submit',classController.getStuHwSubmit);// courses/classes/:classID/student/hw/:hw/submit ->提交作业
+router.get('/class/:classID/student/hw/:hw/situation', classController.getStuHwSituation);// courses/classes/:classID/student/hw/:hw/situation ->提交和批改情况
+router.get('/class/:classID/student/hw/:hw/complain', classController.getStuHwComplain);// courses/classes/:classID/student/hw/:hw/complain ->申诉
+router.post('/class/:classID/student/hw/:hw/submitComplain', classController.submitComplain);//处理申诉
+router.get('/class/:classID/student/hw/:hw/submit', classController.getStuHwSubmit);// courses/classes/:classID/student/hw/:hw/submit ->提交作业
 
 router.get("/class/:classID/teacher/hw/:hw", classController.checkHw);
 router.get("/class/:classID/teacher/hw/all", classController.getTcAllHw);
@@ -50,16 +50,8 @@ router.get("/class/:classID/teacher/hw/add", classController.addHw);
 router.post('/complainBoard', homeworkController.writeComplainBoard);//待定
 router.get("/:courseNumber", courseController.getCourseInfo); //courses/courseNumber
 
-router.post(
-  "/class/:classID/notice/updateNotice",
-  classController.updateNotice
-);
+router.post("/class/:classID/notice/updateNotice", classController.updateNotice);
 router.post("/class/:classID/talk/updateTalk", classController.writeTalk);
-router.post(
-  "/class/:classID/talk/:talkID/updateComment",
-  classController.addComment
-);
-//router.get('/courseGrade',courseController.getGrade);
-//router.get('/courseGradeChange', courseController.gradeChange);
+router.post("/class/:classID/talk/:talkID/updateComment", classController.addComment);
 
 module.exports = router;
