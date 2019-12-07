7ab6f65ac358dcb8095a92af10db140a8b40b192
ycj <1052257712@qq.com>
2019-12-07 18:48:19 +0800

    formidable

---
+++ models/method/fileHelper.js
@@ -0,0 +1,44 @@
+var fs = require("fs");
+var formidable = require("formidable");
+var path = require("path");
+var config = require("../statics/config");
+
+module.exports = (req, name, dirName) => {
+    return new Promise((resolve, reject) => {
+        var form = new formidable.IncomingForm();
+        form.encoding = 'utf-8';
+        form.uploadDir = path.join(__dirname, "../../private", dirName);
+        form.keepExtensions = true;
+        form.maxFileSize = config.FILE_MAX_SIZE; //文件最大大小
+        form.parse(req, (err, params, file) => {
+            if (err) {
+                reject(err.message);
+                return;
+            }
+            if (file[name] == undefined || file[name].size <= 0) {
+                if (file[name] != undefined) {
+                    fs.unlink(file[name].path, (err) => {
+                        reject("请选择上传的文件");
+                        return;
+                    });
+                }
+            } else {
+                var num = file[name].path.lastIndexOf(".");
+                var extension = file[name].path.substr(num).toLowerCase();
+                if (!config.FILE_EXCLUDE_TYPE.includes(extension)) {  //文件扩展名不在黑名单内
+                    var newName = (new Date()).getTime() + extension;  //更改文件名
+                    fs.rename(file[name].path, form.uploadDir + "/" + newName, (err) => {
+                        resolve(newName);
+                        return;
+                    });
+                } else {
+                    fs.unlink(file[name].path, (err) => {
+                        reject("禁止上传" + extension + "格式的文件！");
+                        return;
+                    });
+                }
+            }
+        });
+
+    });
+}
