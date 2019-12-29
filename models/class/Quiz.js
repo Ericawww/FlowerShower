0de590528bf02f8a7074d0dbfe9398f601a82617
LuckyTiger123 <747837496@qq.com>
2019-12-29 14:34:36 +0800

    5 modules in Quiz

--- models/class/Quiz.js
+++ models/class/Quiz.js
@@ -23,25 +23,78 @@ class Quiz {
     }
 
     async insertProblemToQuiz(classProjectID, problemID, score) {
-        
+        try {
+            var conn = await pool.getConnection();
+            await conn.query("insert into project_problem(classProjectID, problemID, score) values (?, ?, ?)", [classProjectID, problemID, score]);
+            return 1;
+        } catch (err) {
+            console.log(err);
+            return 0;
+        } finally {
+            conn.release();
+        }
     }
 
     async removeProblemFromQuiz(classProjectID, problemID) {
-
+        try {
+            var conn = await pool.getConnection();
+            var ret = await conn.query("select * from class_project_score where classProjectID = ?", [classProjectID]);
+            if (ret[0].length != 0) {
+                return -1;
+            }
+            await conn.query("delete from project_problem where classProjectID = ? and problemID = ?", [classProjectID, problemID]);
+            return 1;
+        } catch (err) {
+            console.log(err);
+            return 0;
+        } finally {
+            conn.release();
+        }
     }
 
     async getQuizProblems(classProjectID) {
-
+        try {
+            var conn = await pool.getConnection();
+            var ret = await conn.query("select * from project_problem natural join problem_bank where classProjectID = ?", [classProjectID]);
+            return ret[0];
+        } catch (err) {
+            console.log(err);
+        } finally {
+            conn.release();
+        }
     }
 
     async submitQuiz(answers) {
         let { studentID, classProjectID, mark } = answers;
         //let commitTime = new Date();
         //'insert into .. values(CURRENT_TIMESTAMP)'
+        try {
+            var conn = await pool.getConnection();
+            var ret = await conn.query("insert into class_project_score(classProjectID, studentID, commitTime, mark) values (?, ?, CURRENT_TIMESTAMP, ?)", [classProjectID, studentID, mark]);
+            return 1;
+        } catch (err) {
+            console.log(err);
+            return 0;
+        } finally {
+            conn.release();
+        }
     }
 
     async getUserQuizStatus(studentID, classProjectID) {
-        
+        try {
+            var conn = await pool.getConnection();
+            var ret = await conn.query("select * from class_project_score where classProjectID = ? and studentID = ?", [classProjectID, studentID]);
+            if (ret[0].length > 0) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } catch (err) {
+            console.log(err);
+            return 0;
+        } finally {
+            conn.release();
+        }
     }
 
 }
--- models/class/test.js
+++ models/class/test.js
@@ -1,34 +1,10 @@
 var User = require('./User');
+var Quiz = require('./Quiz');
 var pool = require('../mysql/ConnPool');
-var test = async (classID, studentID) => {
-    try {
-        var conn = await pool.getConnection();
-        var personalMarkResult = 0;
-        var projectGrade;
-        var retFullMark = await conn.query("select sum(fullMark) as totalScore from class_project where classID = ? group by classID", [classID]);
-        var personalMark = await conn.query("select mark from class_project natural join class_project_score where studentID = ? and classID = ?", [studentID, classID]);
-        console.log(retFullMark[0].length);
-        console.log(personalMark[0].length);
-        if (retFullMark[0].length == 0) {
-            projectGrade = 100;
-            return projectGrade;
-        } else {
-            for (var i = 0; i < personalMark[0].length; i++) {
-                personalMarkResult += parseInt(personalMark[0][i].mark);
-            }
-            projectGrade = parseFloat(personalMarkResult * 100 / retFullMark[0][0].totalScore);
-            projectGrade = (projectGrade).toFixed(2);
-            return projectGrade;
-        }
-
-    } catch (err) {
-        console.log(err);
-        return null;
-    } finally {
-        conn.release();
-    }
-    
+var test = async () => {
+    var ret = await Quiz.prototype.getUserQuizStatus("1111", 5);
+    console.log(ret);
 };
 
-test('02', '3170100001');
+test();
 
